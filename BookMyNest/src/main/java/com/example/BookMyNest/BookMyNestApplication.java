d71d32bfacd3f189fc8534c1ee08b6cdfb324eb7
Rohit Kumar <rokumar1519@gmail.com>
2025-08-06 01:53:50 +0530

    add dependencies

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/BookMyNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyNestApplication.class, args);
+	}
+
+}
---
+++ BookMyNest/src/test/java/com/example/BookMyNest/BookMyNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookMyNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
