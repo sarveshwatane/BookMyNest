f3494a2ae6dd710e92c39e19d807ef89b1e51b25
Rohit Kumar <rokumar1519@gmail.com>
2025-08-06 04:27:21 +0530

    created User entity

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/entities/User.java
@@ -0,0 +1,48 @@
+package com.example.BookMyNest.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.example.BookMyNest.enums.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Email is required")
+	@Column(unique = true)
+	private String email;
+	
+	@NotBlank(message = "Phone Number is required")
+	@Column(name = "phone_number")
+	private String phoneNumber;
+	
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
+	
+	private Boolean isActive;
+	private final LocalDateTime createdAt = LocalDateTime.now();
+	
+}
