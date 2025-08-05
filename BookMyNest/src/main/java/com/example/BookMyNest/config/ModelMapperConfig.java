c126ec075c03ac94a35b2a8539b1416128a5c16d
Rohit Kumar <rokumar1519@gmail.com>
2025-08-06 03:48:47 +0530

    Model Mapper configuration

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/config/ModelMapperConfig.java
@@ -0,0 +1,21 @@
+package com.example.BookMyNest.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+		.setFieldMatchingEnabled(true)
+		.setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+		.setMatchingStrategy(MatchingStrategies.STANDARD);
+		
+		return modelMapper;
+	}
+	
+}
