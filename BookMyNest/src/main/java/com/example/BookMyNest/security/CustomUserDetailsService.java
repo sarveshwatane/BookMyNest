159725fdd162e117ac90d0176e212f4e063edd8d
Rohit Kumar <rokumar1519@gmail.com>
2025-08-08 03:29:49 +0530

    add AuthFilter and CustomUserDetailsService for JWT authentication

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.BookMyNest.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.BookMyNest.entities.User;
+import com.example.BookMyNest.exception.NotFoundException;
+import com.example.BookMyNest.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username)
+				.orElseThrow(() -> new NotFoundException("User Email Not Found"));
+		return AuthUser.builder().user(user).build();
+	}
+
+}
