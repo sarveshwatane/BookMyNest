63642a6cccfbfee1c9ceff306d76e72993290e64
Rohit Kumar <rokumar1519@gmail.com>
2025-08-07 06:32:55 +0530

    create User dto

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/dtos/UserDTO.java
@@ -0,0 +1,36 @@
+package com.example.BookMyNest.dtos;
+
+import com.example.BookMyNest.enums.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+
+    private Long id;
+
+    private String email;
+    @JsonIgnore
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+
+    private  UserRole role; 
+
+    private Boolean isActive;
+    private LocalDateTime createdAt;
+}
