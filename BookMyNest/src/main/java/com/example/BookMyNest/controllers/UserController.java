ada54dfadfa73f84748470a299dfb29f71372bb4
sarveshwatane <watanesarvesh@gmail.com>
2025-08-25 02:04:44 +0530

    Add controllers

---
+++ BookMyNest/src/main/java/com/example/BookMyNest/controllers/UserController.java
@@ -0,0 +1,54 @@
+package com.example.BookMyNest.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.BookMyNest.dtos.Response;
+import com.example.BookMyNest.dtos.UserDTO;
+import com.example.BookMyNest.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+
+    @PutMapping("/update")
+    public ResponseEntity<Response> updateOwnAccount(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.updateOwnAccount(userDTO));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<Response> deleteOwnAccount(){
+        return ResponseEntity.ok(userService.deleteOwnAccount());
+    }
+
+    @GetMapping("/account")
+    public ResponseEntity<Response> getOwnAccountDetails(){
+        return ResponseEntity.ok(userService.getOwnAccountDetails());
+    }
+
+
+    @GetMapping("/bookings")
+    public ResponseEntity<Response> getMyBookingHistory(){
+        return ResponseEntity.ok(userService.getMyBookingHistory());
+    }
+
+
+}
